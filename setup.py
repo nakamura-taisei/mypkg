0f14f04b2779294e38151cc41f8827cc2dba6add
taisei nakamura <s23c1098hm@s.chibakoudai.jp>
2025-01-02 18:57:46 +0900

    Add a command

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'mypkg'
@@ -10,16 +12,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='taisei',
     maintainer_email='s23c1098hm@s.chibakoudai.jp',
-    description=,'a package for practice'
-    license=,'BSD-3-Clause'
+    description='a package for practice',
+    license='BSD-3-Clause',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+        'talker = mypkg.talker:main',
+        'listener = mypkg.listener:main',
         ],
     },
 )
