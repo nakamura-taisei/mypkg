38dcc3ee7f3faf6b281fa761c449f22978ce4a11
taisei nakamura <s23c1098hm@s.chibakoudai.jp>
2025-01-04 04:17:38 +0900

    Add a test

--- test/test.bash
+++ test/test.bash
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# デフォルトディレクトリをホームに設定
 dir=~
+# 引数があれば、そのディレクトリに設定
 [ "$1" != "" ] && dir="$1"
 
+# ros2_ws ディレクトリに移動
 cd $dir/ros2_ws
-colcon build
 
-source $dir/.bashrc
+# mypkg パッケージをビルド
+colcon build --packages-select mypkg
 
-timeout 10 ros2 launch mypkg datetime_weather.launch.py > /tmp/mypkg.log'
+# ros2_ws をソース
+source $dir/ros2_ws/install/setup.bash
+
+# datetime_weather.launch.py を実行（10秒間タイムアウトし、ログは /tmp/mypkg.log に保存）
+timeout 10 ros2 launch mypkg datetime_weather.launch.py > /tmp/mypkg.log
+
