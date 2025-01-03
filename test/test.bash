7f49de6d8196836f3eb5302fe74c6653847a0ff1
taisei nakamura <s23c1098hm@s.chibakoudai.jp>
2025-01-04 04:45:35 +0900

    Add a test

--- test/test.bash
+++ test/test.bash
@@ -9,16 +9,9 @@ dir=~
 cd $dir/ros2_ws
 
 # mypkg パッケージをビルド
-echo "Building the package..."
-colcon build --packages-select mypkg
 
-# ビルドが成功したか確認
-if [ $? -ne 0 ]; then
-    echo "Error: colcon build failed."
-    exit 1
-fi
+colcon build
 
 # datetime_weather.launch.py を実行（10秒間タイムアウトし、ログは /tmp/mypkg.log に保存）
-echo "Launching the package..."
-timeout 10 ros2 launch mypkg datetime_weather.launch.py > /tmp/mypkg.log
+ros2 launch mypkg datetime_weather.launch.py > /tmp/mypkg.log
 
