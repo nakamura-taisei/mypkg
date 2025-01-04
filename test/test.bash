24aad3878fee3f9e5b71169f6001dd06779d5aff
taisei nakamura <s23c1098hm@s.chibakoudai.jp>
2025-01-04 11:16:00 +0900

    Add a test

--- test/test.bash
+++ test/test.bash
@@ -12,8 +12,9 @@ cd $dir/ros2_ws
 
 colcon build
 
+source $dir/.bashrc
 # datetime_weather.launch.py を実行（10秒間タイムアウトし、ログは /tmp/mypkg.log に保存）
-timeout 10 ros2 launch mypkg datetime_weather.launch.py 
+timeout 10 ros2 launch mypkg datetime_weather.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep 'datetime_weather.launch.py: 10'
